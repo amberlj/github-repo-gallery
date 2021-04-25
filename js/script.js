df8f95ca32ed6a9200017407a98643f1f2e72ef8
amber_j <amberleigh2448@gmail.com>
2021-04-25 15:36:34 -0400

    Updated error

--- js/script.js
+++ js/script.js
@@ -82,3 +82,4 @@ const displayRepoInfo = function (repoInfo, languages) {
     <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
   `;
   repoData.append(div);
+};
